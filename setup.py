d70443d230cba3cc16339cc1ec4cdaef4c91e8df
Bernat Guillen <bguillen@gmail.com>
2017-01-26 21:16:19 -0500

    Modified setup files and test environments

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup,find_packages
 from codecs import open
 import os
+from setuptools import find_packages
+from setuptools import setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -15,12 +18,12 @@ setup(
 
     version='0.01dev',
     description='Database of food at home',
-#    long_description = read('README.md'),
+    # long_description = read('README.md'),
 
-    url = 'https://github.com/bernatguillen/ChoppingLiszt',
+    url='https://github.com/bernatguillen/ChoppingLiszt',
 
-    author = 'Bernat Guillen',
-    author_email = 'bguillen@gmail.com',
+    author='Bernat Guillen',
+    author_email='bguillen@gmail.com',
     license='GNU GENERAL PUBLIC LICENSE',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -31,9 +34,10 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Topic :: Utilities'
         ],
-    keywords = 'SQLite Food Storage',
-    test_suite =  'nose.collector',
-    tests_require = ['nose'],
-    packages=find_packages(exclude = ['tests*']),
-    #install_requires=['sqlite3']
+    keywords='SQLite Food Storage',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    packages=find_packages(exclude=['tests*']),
+    entry_points={'console_scripts': ['chopp = ChoppingLiszt.database', ], },
+    # install_requires=['sqlite3']
 )
